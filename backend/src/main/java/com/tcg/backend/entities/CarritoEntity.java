69168e040e776cf24cab720e992fdd5892d52a4d
Constanza Rioseco <constanza.rioseco.z@usach.cl>
2025-07-08 00:41:20 -0400

    resolviendo merge

---
+++ backend/src/main/java/com/tcg/backend/entities/CarritoEntity.java
@@ -0,0 +1,47 @@
+package com.tcg.backend.entities;
+
+public CarritoEntity {
+
+    private int id_carro;
+    private int id_metodo_pago;
+    private int id_usuario;
+    private Boolean estado_carrito;
+    private int cantidad_elementos;
+    private num total_pago;
+
+    public BoletaEntity ( int id_carro, int id_metodo_pago, int id_usuario, Boolean estado_carrito,
+                                int cantidad_elementos, num total_pago){
+		this.id_carro = id_carro;
+		this id_metodo_pago = id_metodo_pago;
+		this id_usuario = id_usuario;
+		this estado_carrito = estado_carrito;
+        this.cantidad_elementos = cantidad_elementos;
+		this total_pago = total_pago;
+
+    }
+    public CarritoEntity() {
+        this.id_carro = -1;
+        this id_metodo_pago = -1;
+        this id_usuario = -1;
+        this estado_carrito = null;
+        this.cantidad_elementos = -1;
+        this total_pago = 0;
+    }
+
+    public int getId_carro() {return id_carro;}
+    public void setId_carro(int id_carro) {this.id_carro = id_carro;}
+
+    public int getId_metodo_pago() {return id_metodo_pago;}
+    public void setId_metodo_pago(int id_metodo_pago) {this id_metodo_pago = id_metodo_pago;}
+
+    public int getId_usuario() {return id_usuario;}
+    public void setId_usuario(int id_usuario) {this id_usuario = id_usuario;}
+
+    public int getEstado_carrito() {return estado_carrito;}
+    public void setEstado_carrito(Boolean estado_carrito) {this estado_carrito = estado_carrito;}
+
+    public int getCantidad_elementos() {return cantidad_elementos;}
+    public void setCantidad_elementos(int cantidad_elementos) { this.cantidad_elementos = cantidad_elementos;}
+
+    public int getTotal_pago() {return total_pago;}
+    public void setTotal_pago(num total_pago) {this total_pago = total_pago;}
---
+++ backend/src/main/java/com/tcg/backend/entities/MetodosPagoEntity.java
@@ -0,0 +1,59 @@
+package com.tcg.backend.entities;
+
+public class MetodosPagoEntity {
+
+    private int id_metodo_de_pago;
+    private String tipo_de_pago;
+    private String banco;
+    private String nombre_titular;
+    private int numero_tarjeta;
+    private Date fecha_vencimiento;
+    private int codigo_seguridad;
+    private int rut_titular;
+
+    public class BoletaEntity (int id_metodo_de_pago, String tipo_de_pago, String banco, String nombre_titular,
+                               int numero_tarjeta, Date fecha_vencimiento, int codigo_seguridad, int rut_titular) {
+		this.id_metodo_de_pago =id_metodo_de_pago;
+		this tipo_de_pago =tipo_de_pago;
+		this banco =banco;
+		this nombre_titular =nombre_titular;
+		this numero_tarjeta =numero_tarjeta;
+        this fecha_vencimiento =fecha_vencimiento;
+        this codigo_seguridad =codigo_seguridad;
+        this rut_titular =rut_titular;
+    }
+    public MetodosPagoEntity() {
+        this.id_metodo_de_pago = -1;
+        this.tipo_de_pago = null;
+        this.banco = null;
+        this.nombre_titular = null;
+        this.numero_tarjeta = -1;
+        this.fecha_vencimiento = 0;
+        this.codigo_seguridad = -1;
+        this.rut_titular = -1;
+    }
+
+
+    public int getId_metodo_de_pago() {return id_metodo_de_pago;}
+    public void setId_metodo_de_pago(int id_metodo_de_pago) {this.id_metodo_de_pago =id_metodo_de_pago;}
+
+    public int getTipo_de_pago() {return tipo_de_pago;}
+    public void setTipo_de_pago(String tipo_de_pago) {this tipo_de_pago =tipo_de_pago;}
+
+    public int getBanco() {return banco;}
+    public void setBanco(String banco) {this banco =banco;}
+
+    public int getNombre_titular() {return nombre_titular;}
+    public void setNombre_titular(String nombre_titular) {this nombre_titular =nombre_titular;}
+
+    public int getNumero_tarjeta() {return numero_tarjeta;}
+    public void setuNmero_tarjeta(int numero_tarjeta) {this numero_tarjeta =numero_tarjeta;}
+
+    public int getFecha_vencimiento() {return fecha_vencimiento;}
+    public void setFecha_vencimiento(Date fecha_vencimiento) { this fecha_vencimiento =fecha_vencimiento;}
+
+    public int getCodigo_seguridad() {return codigo_seguridad;}
+    public void setCodigo_seguridad(int codigo_seguridad) {this codigo_seguridad =codigo_seguridad;}
+
+    public int getRut_titular() {return rut_titular;}
+    public void setRut_titular(int rut_titular) {this rut_titular =rut_titular;}
