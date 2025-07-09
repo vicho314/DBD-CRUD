7dffb22556464a118035d28dec9b83357b69ab76
alchemina <paulina.guasch@usach.cl>
2025-07-09 00:52:12 -0400

    'cartaylistarepository'

---
+++ backend/src/main/java/com/tcg/backend/repositories/CartaRepository.java
@@ -0,0 +1,64 @@
+package com.tcg.backend.repositories;
+
+import com.tcg.backend.entities.CartaEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+@Repository
+public class CartaRepository{
+    @Autowired
+    private Sql2o sql2o;
+
+    public List<CartaEntity> findAll(){
+        String sql = "SELECT * FROM carta";
+        try (Connection con = sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(CartaEntity.class);
+        }
+    }
+
+    public CartaEntity findById(int id){
+        String sql = "SELECT * FROM carta WHERE id_carta = :id_carta";
+        try (Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .addParameter("id_carta", id)
+                    .executeAndFetchFirst(CartaEntity.class);
+        }
+    }
+    public CartaEntity save (CartaEntity carta){
+        String sql = """
+                INSERT INTO carta (posicion_deseada, precio, stock, rareza, año, estado, id_producto)
+                VALUES (:posicion_deseada, :precio, :stock, :rareza, :año, :estado, :id_producto)
+        """;
+        try (Connection con = sql2o.open()){
+            int id = con.createQuery(sql, true)
+                    .addParameter("posicion_deseada", carta.getPosicion_deseada())
+                    .addParameter("precio",  carta.getPrecio())
+                    .addParameter("stock", carta.getStock())
+                    .addParameter("rareza", carta.getRareza())
+                    .addParameter("año", carta.getAño())
+                    .addParameter("estado", carta.getEstado())
+                    .addParameter("id_producto", carta.getId_producto())
+                    .executeUpdate()
+                    .getKey(Integer.class);
+
+            carta.setId_carta(id);
+            return carta;
+        }
+    }
+
+    public boolean delete(int id){
+        String sql = "DELETE FROM carta WHERE id_carta = :id_carta";
+        try (Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("id_carta", id)
+                    .executeUpdate();
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
---
+++ backend/src/main/java/com/tcg/backend/repositories/ListaDeseosRepository.java
@@ -0,0 +1,59 @@
+package com.tcg.backend.repositories;
+
+import com.tcg.backend.entities.ListaDeseosEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
+@Repository
+public class ListaDeseosRepository{
+    @Autowired
+    private Sql2o sql2o;
+
+    public List<ListaDeseosEntity> findAll(){
+        String sql = "SELECT * FROM lista_de_deseos";
+        try  (Connection con = sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(ListaDeseosEntity.class);
+        }
+    }
+
+    public ListaDeseosEntity findById(int id){
+        String sql = "SELECT * FROM lista_de_deseos WHERE id_lista = :id_lista";
+        try  (Connection con = sql2o.open()) {
+            return con.createQuery(sql)
+                    .addParameter("id_lista", id)
+                    .executeAndFetchFirst(ListaDeseosEntity.class);
+        }
+    }
+
+    public ListaDeseosEntity save(ListaDeseosEntity lista) {
+        String sql = """
+                INSERT INTO lista_de_deseos (nombre_lista)
+                VALUES (:nombre_lista)
+        """;
+        try  (Connection con = sql2o.open()) {
+            int id = con.createQuery(sql, true)
+                    .addParameter("nombre_lista", lista.getNombre_lista())
+                    .executeUpdate()
+                    .getKey(Integer.class);
+
+            lista.setId_lista(id);
+            return lista;
+        }
+    }
+
+    public boolean delete(int id) {
+        String sql = "DELETE FROM lista_de_deseos WHERE id_lista = :id_lista";
+        try  (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id_lista", id)
+                    .executeUpdate();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
