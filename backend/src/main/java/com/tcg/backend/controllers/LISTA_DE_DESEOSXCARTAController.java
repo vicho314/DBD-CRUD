e39a0bf48f3ae2378205e0e822ff970008316fd4
JesusTorres16 <jesus.torres@usach.cl>
2025-07-09 20:43:55 -0400

    CORRECCION ldxcCONTROLLER

--- backend/src/main/java/com/tcg/backend/controllers/LISTA_DE_DESEOSXCARTAController.java
+++ backend/src/main/java/com/tcg/backend/controllers/LISTA_DE_DESEOSXCARTAController.java
@@ -10,30 +10,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/ListaDeseosXCarta")
 @CrossOrigin("*")
-public class ListaDeseosXCartaController {
+public class LISTA_DE_DESEOSXCARTAController {
     @Autowired
-    private LISTA_DE_DESEOSXCARTAService listaDeseosXCartaService;
+    private LISTA_DE_DESEOSXCARTAService LISTA_DE_DESEOSXCARTAService;
 
     @GetMapping("/")
     public List<LISTA_DE_DESEOSXCARTAEntity> getAll() {
-        return listaDeseosXCartaService.getAll();
+        return LISTA_DE_DESEOSXCARTAService.getAll();
     }
 
-    @GetMapping("{idLista}/{idCarta}")
-    public LISTA_DE_DESEOSXCARTAEntity getById(@PathVariable int idLista, @PathVariable int idCarta) {
-        return listaDeseosXCartaService.getById(idLista, idCarta);
+    @GetMapping("{id_lista}/{id_carta}")
+    public LISTA_DE_DESEOSXCARTAEntity getById(@PathVariable int id_lista, @PathVariable int id_carta) {
+        return LISTA_DE_DESEOSXCARTAService.getById(id_lista, id_carta);
     }
 
     @PostMapping("/save")
     public LISTA_DE_DESEOSXCARTAEntity save(@RequestBody LISTA_DE_DESEOSXCARTAEntity entity) {
-        return listaDeseosXCartaService.save(entity);
+        return LISTA_DE_DESEOSXCARTAService.save(entity);
     }
-    @DeleteMapping("/delete/{idLista}/{idCarta}")
-    public boolean delete(@PathVariable int idLista, @PathVariable int idCarta) {
-        return listaDeseosXCartaService.delete(idLista, idCarta);
+    @DeleteMapping("/delete/{id_lista}/{id_carta}")
+    public boolean delete(@PathVariable int id_lista, @PathVariable int id_carta) {
+        return LISTA_DE_DESEOSXCARTAService.delete(id_lista, id_carta);
     }
     @PutMapping("/updateCantidad")
-    public boolean updateCantidad(@RequestParam int idLista, @RequestParam int idCarta, @RequestParam int nuevaCantidad) {
-        return listaDeseosXCartaService.updateCantidad(idLista, idCarta, nuevaCantidad);
+    public boolean updateCantidad(@RequestParam int id_lista, @RequestParam int id_carta, @RequestParam int nuevaCantidad) {
+        return LISTA_DE_DESEOSXCARTAService.updateCantidad(id_lista, id_carta, nuevaCantidad);
     }
 }
